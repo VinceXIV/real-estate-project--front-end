58e87afd0374f52415635266d6824dd56625dcc2
Zablon-Onchari-Nyabuto <zablon.nyabuto@student.moringaschool.com>
2022-11-09 18:08:11 +0300

    Added form on the sell page for seller information on the house

--- src/pages/Rent.js
+++ src/pages/Rent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
-import Card from './Card'
+import Card from './Card';
 
 function Rent() {
     const [houses, setHouses] = useState([]);
@@ -17,26 +17,26 @@ function Rent() {
             <h1>Rental Listings</h1>
             <ul>
                 Looking for a House to Rent? Search here for your taste...
-            <div className="search">
-                <TextField
-                    id="outlined-basic"
-                    variant="outlined"
-                    fullWidth
-                    label="Search"
-                />
-            </div>
+                <div className="search">
+                    <TextField
+                        id="outlined-basic"
+                        variant="outlined"
+                        fullWidth
+                        label="Search"
+                    />
+                </div>
             </ul>
             <div>
                 <div>
                     <h1>Popular Houses</h1>
+                    {houses.map((house) =>
+                        <Card house={house} key={house.id} />
+                    )}
                 </div>
-            <div>
-            { houses.map((house)=> 
-            <Card house={house} key={house.id} />
-            )}
-        </div>
             </div>
+
         </div>
+
     );
 }
 export default Rent;
