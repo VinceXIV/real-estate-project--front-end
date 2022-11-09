073ae4fafe9c34421fefcb094e880e708799f2b9
Vincent Makokha <makokhavomondi@gmail.com>
2022-11-09 18:10:11 +0300

    Enabled filtering by location

--- src/pages/LocationCategory.js
+++ src/pages/LocationCategory.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Card from "./Card";
+import Locations from "./Locations";
 
 function LocationCategory(){
-    const [selectedCategory, setSelectedCategory] = useState([])
-    const [houses, setHouses] = useState([])
-    const [locations, setLocations] = useState([])
+    const [allHouses, setAllHouses] = useState([])
+    const [displayedHouses, setDisplayedHouses] = useState([])
     const apiHost = "http://localhost:9292"
 
     const imageCSS = {
@@ -23,42 +23,29 @@ function LocationCategory(){
     useEffect(()=>{
         fetch(`${apiHost}/houses`)
         .then((responce)=>responce.json())
-        .then((data)=>setHouses(data))
+        .then((data)=>{
+          setAllHouses(data)
+          setDisplayedHouses(data)
+        })
     },[])
-    useEffect(()=>{
-        fetch(`${apiHost}/locations`)
-        .then((responce)=>responce.json())
-        .then((data)=>setLocations(data))
-
-    },[])
-    console.log(locations)
-    console.log(houses)
-
 
     function filterHandler(e){
-      setSelectedCategory(e.target.value) 
+      setDisplayedHouses(displayedHouses => {
+        return (
+          allHouses.filter(house => house.location == e.target.value)
+        )
+      })
     }
-    const displayHouses = houses.filter((house)=>{
-      if (selectedCategory === "All"){
-        return true;
-      }else{
-        return house.location_id === selectedCategory
-      }
-    })
+   
 
     return(
         <>
         <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select a Region</label>
             <select onChange={filterHandler} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                <option defaultValue={0}>Choose a Region</option>
-                <option value="All">All Houses"</option>
-                <option value="11">Kisumu"</option>
-                <option value="10">Kakamege</option>
-                <option value="8">Nairobi</option>
-                <option value="9">Mombasa</option>
+            <Locations houses={allHouses} displayedHouses={displayedHouses}/>
             </select>
         <div style={imageCSS}>
-        {displayHouses.map((house) => (
+        {displayedHouses.map((house) => (
           <Card key={house.id} house={house}/>
         ))} 
         </div>
---
+++ src/pages/Locations.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Locations({houses, displayedHouses}){
+
+    // Get uniq locations
+    const location_info = houses.map(house => {
+        return {value: house.location}
+    })
+
+    const uniq_locations = []
+
+    location_info.forEach(location => {
+        if(!uniq_locations.find(uniq_location => uniq_location.value == location.value)){
+            uniq_locations.push(location)
+        }
+    })
+
+    console.log("categories: ", uniq_locations)
+    // Use the uniq locations to create options
+    const location_options = uniq_locations.map(location => {
+        return <option value={location.value}>{location.value}</option>
+    })
+
+    return (
+        <>
+            {location_options}
+        </>
+    )
+}
+
+export default Locations
