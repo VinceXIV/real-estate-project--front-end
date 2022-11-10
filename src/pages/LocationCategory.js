78139d3451ddcecf213a892482cc38e0edc96e9b
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-10 15:31:42 +0300

    add select styling

--- src/pages/LocationCategory.js
+++ src/pages/LocationCategory.js
@@ -32,7 +32,7 @@ function LocationCategory(){
     function filterHandler(e){
       setDisplayedHouses(displayedHouses => {
         return (
-          allHouses.filter(house => house.location == e.target.value)
+          allHouses.filter(house => house.location === e.target.value)
         )
       })
     }
@@ -40,15 +40,18 @@ function LocationCategory(){
 
     return(
         <>
-        <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Select a Region</label>
-            <select onChange={filterHandler} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <Locations houses={allHouses} displayedHouses={displayedHouses}/>
-            </select>
-        <div style={imageCSS} className="flex flex-wrap">
-          {displayedHouses.map((house) => (
-            <Card key={house.id} house={house}/>
-          ))} 
-        </div>
+          <div className="flex flex-col items-center lg:mx-32 md:mx-16 mx-8">
+              <label  className="block mb-4 lg:text-3xl md:text-2xl text-lg font-medium text-blue-500">Select Properties By Region:</label>
+              <select onChange={filterHandler} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-3 px-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <Locations houses={allHouses} displayedHouses={displayedHouses}/>
+              </select>
+          </div>
+
+          <div style={imageCSS} className="flex flex-wrap">
+            {displayedHouses.map((house) => (
+              <Card key={house.id} house={house}/>
+            ))} 
+          </div>
         </>
     )
 }
