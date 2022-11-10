031f9882c67294dda4c6845ce2b8395e731fb800
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-11-10 21:08:19 +0300

    add hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import HeroImg from "../assets/hero.jpg"
+// import HeroImg from "../assets/hero.jpg"
 
 const Hero = () => {
     return ( 
@@ -8,14 +8,14 @@ const Hero = () => {
                 <div className="">
                     <img 
                         className="hero-image rounded-md" 
-                        src={HeroImg} 
+                        src="http://www.slidebackground.com/uploads/real-estate-background/real-estate-homebuy-home-banner-background-homebuym-1.jpg" 
                         alt="Hero here.." />
                 </div>
                 <div className=" flex flex-col justify-center">
                     <h1 className="title-on-image lg:text-7xl md:text-4xl text-3xl text-gray-800 font-bold md:mb-32 mb-12 ">Discover a place you'll love to live</h1>
                     <div className="text-on-image absolute md:space-x-4 md:text-2xl text-xl  text-center font-medium space-x-4">
                         <Link
-                            className="bg-blue-400 hover:bg-blue-500 hover:text-gray-100 md:px-8 md:py-4 px-4 py-2 rounded-md" to='/buy'>
+                            className="bg-blue-700 hover:bg-white hover:text-gray-900 md:px-8 md:py-4 px-4 py-2 rounded-md" to='/buy'>
                             Buy 
                         </Link>
                         <Link
